de6f54bd32ac47b982eb82d51827b501423d4927
Gagan Bhusal <bhusalgagan612@gmail.com>
2025-04-21 14:59:30 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace teacherMgmtfront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
