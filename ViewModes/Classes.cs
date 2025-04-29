1827623ef1ab33a6734cfae972d3022a3f2f3bca
Gagan Bhusal <bhusalgagan612@gmail.com>
2025-04-29 21:17:28 +0545

    managing class

--- ViewModes/Classes.cs
+++ ViewModes/Classes.cs
@@ -2,5 +2,11 @@
 {
     public class Classes
     {
+        public int Id { get; set; }
+        public string ClassName { get; set; }
+        public string Shift { get; set; }
+        public TimeOnly Duration { get; set; }
+
+        public TimeOnly BreakDuration { get; set; }
     }
 }
--- ViewModes/Subject.cs
+++ ViewModes/Subject.cs
@@ -2,11 +2,6 @@
 {
     public class Subject
     {
-        public int Id { get; set; }
-        public string ClassName { get; set; }
-        public string  Shift{ get; set; }
-        public TimeOnly Duration { get; set; }
-
-        public TimeOnly BreakDuration { get; set; }
+        
     }
 }
