39397db4a956c43b1c64cf1789ea170d38aed4be
Gagan Bhusal <bhusalgagan612@gmail.com>
2025-04-29 21:32:39 +0545

    models

--- ViewModes/Classes.cs
+++ ViewModes/Classes.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string ClassName { get; set; }
         public string Shift { get; set; }
+        public string Stream { get; set; }
         public TimeOnly Duration { get; set; }
 
         public TimeOnly BreakDuration { get; set; }
--- ViewModes/Subject.cs
+++ ViewModes/Subject.cs
@@ -2,6 +2,12 @@
 {
     public class Subject
     {
-        
+        public int Id { get; set; }
+        public string SubjectCode { get; set; }
+        public string SubjectName { get; set; }
+        public string CreditHours { get; set; }
+        public string Department { get; set; }
+        public string ClassType { get; set; }
+
     }
 }
